f346698f6d01c6235c65307d110c22b3fd694f9a
LimuruTempestBest” <limurutempest666@gmail.com>
2022-07-31 20:09:11 +0800

    ShirtStore

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
                 .fill(0)
                 .map(() => (
                   <div>
-                    <Icon icon="fluent:star-12-filled" className="text-yellow-300 w-2 h-2 md:w-4 h-4 mt-1" />
+                    <Icon icon="fluent:star-12-filled" className="text-yellow-300 w-3 h-3 md:w-4 h-4 mt-1" />
                   </div>
                 ))}
 
@@ -33,20 +33,21 @@ function App() {
             <div className="font-medium text-sm md:text-base mt-1 md:mt-3 font-['Ubuntu']">
               Colour
             </div>
-            <div class="inline-flex rounded-md shadow-sm mt-1 md:mt-2" role="group">
-              <button type="button" className="font-['Abel'] w-12 md:w-20 h-8 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 
-              hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-                White
-              </button>
-              <button type="button" className="font-['Abel'] w-12 md:w-20 h-8 text-sm font-medium text-gray-900 bg-transparent rounded-r-lg  border border-gray-900
-              hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-                Black
-              </button>
+
+            <div className="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
+              <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="accent-orange-300 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="bordered-radio-1" className="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300 font-['Abel']" >White</label>
             </div>
-            <div className="font-medium mt-2 md:mt-5 text-sm md:text-base font-['Ubuntu']" role="button">
+            <div className="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
+              <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="accent-orange-300 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="bordered-radio-1" className="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300 font-['Abel']">Black</label>
+            </div>
+
+
+            <div className="font-medium mt-2 md:mt-5 text-sm md:text-base font-['Ubuntu']">
               Size
             </div>
-            <div class="inline-flex rounded-md shadow-sm mt-1 md:mt-2" role="group">
+            <div class="inline-flex rounded-md shadow-sm mt-1 md:mt-2" role="group" aria-Label="Size Group">
 
               <button type="button" class="font-['Abel'] w-11 md:w-12 h-8 md:h-10 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                 XXXS
@@ -63,7 +64,7 @@ function App() {
             </div>
 
             <div>
-              <div class="inline-flex rounded-md shadow-sm mt-2" role="group">
+              <div class="inline-flex rounded-md shadow-sm mt-2" role="group" aria-Label="Size Group">
                 <button type="button" class="font-['Abel'] w-11 md:w-12 h-8 md:h-10 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                   L
                 </button>
@@ -78,11 +79,11 @@ function App() {
                 </button>
               </div>
             </div>
-            
+
             <button className="font-['Ubuntu'] rounded font-semibold  text-white bg-blue-600 w-24 h-8 md:w-40 h-11 mt-10 hover:animate-bounce hover:bg-sky-900">
-              Add to bag 
+              Add to bag
             </button>
-            
+
 
           </div>
 
